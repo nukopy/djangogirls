87afbd229c6dab292544d8596b8c3d1a04e37412
Yosuke Okuwaki <pytwbf201830@gmail.com>
2019-08-22 01:08:19 +0900

    Modified Python typing for Python 3.7.3(local env) -> 3.5.6(deploy env).

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .models import Post
 
 
 def post_list(request):
-    posts: models.Model = Post.objects \
+    posts = Post.objects \
         .filter(published_at__lte=timezone.now()) \
         .order_by('published_at')
     return (render(request, 'blog/post_list.html', {

26a8440a2c6784f09d3b837edaf20db02404b0c0
Yosuke Okuwaki <pytwbf201830@gmail.com>
2019-08-21 21:39:04 +0900

    Changed the HTML for the site.

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # これでDjangoは'http://127.0.0.1:8000/' に来たリクエストは blog.urls へリダイレクトするようになる．を参照するようになる．
+    path('', include('blog.urls')),
 ]
